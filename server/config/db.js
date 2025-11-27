bacd1ffe9d40c3328603b8896be61e8ab845e607
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-11-27 20:15:20 +0530

    initalizing .env and switching from commonjs to modulejs

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
+export const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URL);
         console.log("mongo connected successfully");
@@ -10,4 +10,3 @@ const connectDB = async () => {
     }
 };
 
-module.export = connectDB;
