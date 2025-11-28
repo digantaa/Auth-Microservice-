4dd3cd292c8edb6c58ae1bc05eaee99cf8914cb4
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-11-28 08:59:40 +0530

    backend complete

--- server/models/user.js
+++ server/models/user.js
@@ -3,18 +3,18 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name:{
         type: String,
-        require: true
+        required: true
     },
 
     email:{
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
 
     password:{
         type: String,
-        require: true,
+        required: true,
     },
     
     role: {
