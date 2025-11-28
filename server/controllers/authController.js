7ecdb05089c848e46a830759a3528ce0624cd554
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-11-28 09:42:54 +0530

    adding reset password controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -66,5 +67,67 @@ export const login = async (req, res) => {
 
     }catch(error){
         res.status(500).json({error: error.message});
+    }
+}
+
+export const forgetPassword = async (req, res) => {
+    try{
+
+        const {email} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: "Email not found "})
+
+        //Generate reset token
+        const resetToken = crypto.randomBytes(32).toString("hex");
+
+        //Hash token before saving in DB
+        const hashedToken = crypto.createHahs("sha256")
+            .update(resetToken)
+            .digest("hex");
+        
+        user.resetPasswordToken = hashedToken;
+        user.resetPasswordToken = Date.now() +10*60*1000; //10 minute
+
+        await user.save();
+
+        res.json({
+            message:"Password reset token generated",
+            resetToken
+        })
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+}
+
+export const resetPassword = async (req, res) => {
+    try{
+        const {token, newPassword} = req.body;
+
+        const hashedToken = crypto
+            .createHash("sha256")
+            .update(token)
+            .digest("hex")
+
+        const user = await user.findOne({
+            resetPasswordToken: hashedToken,
+            resetPasswordExpires: {$gt: Date.now()}
+        });
+
+        if(!user) return res.status(400).json({message: "Invalid or expired token"});
+
+        //Update password here
+        user.password = await bcrypt.hash(newPassword,10);
+
+        //clear field
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+
+        await user.save();
+
+
+        res.json({message: "password reset was a success"});
+    }catch(err){
+        res.status(500).json({error: err.message});
     }
 }
