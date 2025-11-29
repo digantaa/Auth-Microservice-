e8f92c2c0c235c3b137d6d79411be27b6fa2f241
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-11-29 12:16:03 +0530

    fixing minor bugs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import { useState }from 'react';
+import { useNavigate, Link} from "react-router-dom";
+import api from '../utils/axiosInstance';
+import {useForm} from 'react-hook-form';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [serverMsg, setServerMsg] = useState("");
+
+  const {
+    register, 
+    handleSubmit,
+    formState:{error, isSubmitting},
+  } = useForm()
+
+  const onSubmit = async(data) =>{
+    setServerMsg("");
+
+    try{
+      const res = await api.post('/auth/login',data);
+
+      localStorage.setItem('token',res.data.accessToken);
+      localStorage.setItem('refreshToken',res.data.refreshToken);
+
+      navigate('/dashboard');
+    }catch(err){
+      setServerMsg(err.response?.data?.message || 'Login failed');
+    }
+  };
+
   return (
-    <div>
-      
+    <div className='p-6 mx-w-sm mx-auto'>
+      <h1 className="text-2xl font-bold mb-4">Login</h1>
+
+      {serverMsg && (
+        <p className="text-rose-500 mb-2">{serverMsg}</p>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+
+        <div>
+          <input type="email" placeholder='Email' className="border p-2 w-full"
+          {...register('email',{
+            required: "Email is required",
+          })} 
+          />
+          {error.email && (
+            <p className="text-rose-500  text-sm">{error.email.messages}</p>
+          )}
+        </div>
+
+           <div>
+          <input
+            type="password"
+            placeholder="Password"
+            className="border p-2 w-full"
+            {...register("password", {
+              required: "Password is required",
+            })}
+          />
+          {error.password && (
+            <p className="text-red-500 text-sm">{error.password.message}</p>
+          )}
+        </div>
+        
+        <button className="bg-stone-600 text-white p-2 rounded mt-2 disabled:bg-stone-300"
+        disabled={isSubmitting}
+        >
+          {isSubmitting?"Logging in..." : "Login"}
+        </button>
+      </form>
+      <div className="mt-4 flex flex-col gap-1">
+        <Link to='/signup' className="text-rose-700 underline">Create Accound</Link>
+        <Link to='/forget' className="text-rose-700 underline">Forget Password?</Link>
+      </div>
     </div>
   )
 }
--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,9 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({
-
-    baseURL: "http://localhost:5000", 
-
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000",
 });
 
-export default api;
+// Automatically attach access token
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default axiosInstance;
