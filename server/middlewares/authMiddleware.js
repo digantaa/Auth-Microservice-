8d9d6931d306c8afbd0811da1e908c5b46e9fb40
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-11-29 11:41:53 +0530

    backend set

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,20 +1,22 @@
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+    try {
+        const authHeader = req.headers.authorization;
 
-    if(!authHeader) return res.status(401).json({message: "No token provided"});
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ message: "No token provided" });
+        }
 
-    const token = authHeader.split(" ")[1];
+        const token = authHeader.split(" ")[1];
 
-    try{
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = decode; //attach user info to request
+        req.user = decoded; // Attach user data
 
-        next(); 
+        next();
 
-    }catch(err){
-        return res.status(401).json({message: "Invalid or expired token"})
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid or expired token" });
     }
 };
